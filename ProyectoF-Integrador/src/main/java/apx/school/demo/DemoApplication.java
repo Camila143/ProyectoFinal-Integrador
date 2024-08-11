96362b022046b788f669b4bff315bea1176879ea
Camila143 <123189295+Camila143@users.noreply.github.com>
2024-08-11 15:35:07 -0600

    Proyecto-Final

---
+++ ProyectoF-Integrador/src/main/java/apx/school/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package apx.school.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("");
+		System.out.println(" =ccccc,      ,cccc       ccccc      ,cccc,  ?$$$$$$$,  ,ccc,   -ccc        \n" +
+				":::\"$$$$bc    $$$$$     ::`$$$$$c,  : $$$$$c`:\"$$$$???'`.\"$$$$c,:`?$$c      \n" +
+				"`::::\"?$$$$c,z$$$$F     `:: ?$$$$$c,`:`$$$$$h`:`?$$$,` :::`$$$$$$c,\"$$h,    \n" +
+				"  `::::.\"$$$$$$$$$'    ..,,,:\"$$$$$$h, ?$$$$$$c`:\"$$$$$$$b':\"$$$$$$$$$$$c   \n" +
+				"     `::::\"?$$$$$$    :\"$$$$c:`$$$$$$$$d$$$P$$$b`:`?$$$c : ::`?$$c \"?$$$$h, \n" +
+				"       `:::.$$$$$$$c,`::`????\":`?$$$E\"?$$$$h ?$$$.`:?$$$h..,,,:\"$$$,:.\"?$$$c \n" +
+				"         `: $$$$$$$$$c, ::``  :::\"$$$b `\"$$$ :\"$$$b`:`?$$$$$$$c``?$F `:: \":: \n" +
+				"          .,$$$$$\"?$$$$$c,    `:::\"$$$$.::\"$.:: ?$$$.:.???????\" `:::  ` ``` \n" +
+				"          'J$$$$P'::\"?$$$$h,   `:::`?$$$c`::``:: .:: : :::::''   `          \n" +
+				"         :,$$$$$':::::`?$$$$$c,  ::: \"::  ::  ` ::'   ``                    \n" +
+				"        .'J$$$$F  `::::: .::::    ` :::'  `                                 \n" +
+				"       .: ???):     `:: :::::                                               \n" +
+				"       : :::::'        `                                                    ");
+
+		System.out.println("El programa está corriendo");
+	}
+
+}
